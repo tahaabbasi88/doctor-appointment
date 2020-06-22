c53bda06b3c81a8e0e3bea133ef666bedcd59635
Taha Abbasi <taha.abbasi88@gmail.com>
2020-06-21 23:27:38 -0400

    added a patient model, left sex/gender column out, need to check how to create enums

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_024800) do
+ActiveRecord::Schema.define(version: 2020_06_22_032623) do
 
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "specialty"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
